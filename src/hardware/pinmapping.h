0df6bc1c159f35041fd2a04d5c2d5308796198d9
banoz <banoz@users.noreply.github.com>
2025-05-01 20:31:21 -0500

    mapped to CleverBoard v1, keine panik

--- src/hardware/pinmapping.h
+++ src/hardware/pinmapping.h
@@ -12,41 +12,41 @@
  */
 
 // Switches/Buttons
-#define PIN_POWERSWITCH 39
-#define PIN_BREWSWITCH  34
-#define PIN_STEAMSWITCH 35
-#define PIN_WATERSWITCH 36
+#define PIN_POWERSWITCH 255
+#define PIN_BREWSWITCH  22
+#define PIN_STEAMSWITCH 21
+#define PIN_WATERSWITCH 255
 
-#define PIN_ROTARY_DT  4 // Rotary encoder data pin
-#define PIN_ROTARY_CLK 3 // Rotary encoder clock pin
-#define PIN_ROTARY_SW  5 // Rotary encoder switch
+#define PIN_ROTARY_DT  255 // Rotary encoder data pin
+#define PIN_ROTARY_CLK 255 // Rotary encoder clock pin
+#define PIN_ROTARY_SW  255 // Rotary encoder switch
 
 // Sensors
-#define PIN_TEMPSENSOR      16
-#define PIN_WATERTANKSENSOR 23
-#define PIN_HXDAT           32 // Brew scale data pin 1
-#define PIN_HXDAT2          25 // Brew scale data pin 2
-#define PIN_HXCLK           33 // Brew scale clock pin
+#define PIN_TEMPSENSOR      3
+#define PIN_WATERTANKSENSOR 255
+#define PIN_HXDAT           19 // Brew scale data pin 1
+#define PIN_HXDAT2          21 // Brew scale data pin 2
+#define PIN_HXCLK           20 // Brew scale clock pin
 
 /**
  * Output pins
  */
 
 // Relays
-#define PIN_VALVE  17
-#define PIN_PUMP   27
-#define PIN_HEATER 2
+#define PIN_VALVE  15
+#define PIN_PUMP   18
+#define PIN_HEATER 10
 
 // LEDs
-#define PIN_STATUSLED 26
-#define PIN_BREWLED   19
-#define PIN_STEAMLED  1
+#define PIN_STATUSLED 255
+#define PIN_BREWLED   255
+#define PIN_STEAMLED  255
 
 // Periphery
-#define PIN_ZC 18 // Dimmer circuit Zero Crossing
+#define PIN_ZC 0 // Dimmer circuit Zero Crossing
 
 /**
  * Bidirectional Pins
  */
-#define PIN_I2CSCL 22
-#define PIN_I2CSDA 21
+#define PIN_I2CSCL 7
+#define PIN_I2CSDA 6
